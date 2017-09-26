33acadc236be9e6775aa6ecee4b87b34fa9e0e3f
Adriyadi Savana <adrie4mac@gmail.com>
2017-09-26 15:25:11 +0700

    [ADD] Add progurad glide

--- app/src/main/java/id/web/adrie/gallery/gallerylist/GalleryListItem.kt
+++ app/src/main/java/id/web/adrie/gallery/gallerylist/GalleryListItem.kt
@@ -2,7 +2,6 @@ package id.web.adrie.gallery.gallerylist
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.mikepenz.fastadapter.items.AbstractItem
 import id.web.adrie.gallery.R
@@ -13,6 +12,12 @@ import kotlinx.android.synthetic.main.item_gallery.view.*
  * Created by adrie_savana on 9/25/17.
  */
 class GalleryListItem(val name: String): AbstractItem<GalleryListItem, GalleryListItem.ViewHolder>() {
+
+    private var mViewHolder: ViewHolder? = null
+
+    init {
+
+    }
 
     override fun getViewHolder(v: View): ViewHolder {
         return ViewHolder(v)
@@ -28,7 +33,7 @@ class GalleryListItem(val name: String): AbstractItem<GalleryListItem, GalleryLi
 
     override fun bindView(holder: ViewHolder, payloads: MutableList<Any>?) {
         super.bindView(holder, payloads)
-        //holder.name.text = name
+        mViewHolder = holder
         Glide.with(holder.itemView.context)
                 .load("https://mostodd.files.wordpress.com/2011/01/types-of-goldfish.jpg")
                 .into(holder.image)
@@ -36,20 +41,12 @@ class GalleryListItem(val name: String): AbstractItem<GalleryListItem, GalleryLi
 
     override fun unbindView(holder: ViewHolder?) {
         super.unbindView(holder)
-        //holder!!.name.text = null
     }
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        //var name = itemView.textName
-        var image = itemView.imgImagePhoto
-
-        init {
-            val height = DeviceUtil.getScreenWith(itemView.context)
-            image.layoutParams.height = height / 2
-            image.layoutParams.width = height / 2
+        var image = itemView.imgImagePhoto.apply {
+            layoutParams.height = DeviceUtil.getScreenWith(itemView.context) / 2
+            layoutParams.width = layoutParams.height
         }
-
-
     }
-
 }
